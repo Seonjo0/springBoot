da331b5d0371c9a6b82ee7152ce1ab0709ac73ee
Seonjo0 <129243915+Seonjo0@users.noreply.github.com>
2024-10-24 13:26:17 +0900

    Study : Component Scan / Auto dependency

---
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    // Autowired: 기존 컨테이너에 있는 MemberService를 가져다가 연결해줌. 스프링 컨테이너에서 이미 생성돼있는 것을 가져다 줌 => 매번 객체 새로 생성하지 않음
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemoryMemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemoryMemberRepository memberRepository1) {
         this.memberRepository = memberRepository1;
     }
@@ -36,6 +40,5 @@ public class MemberService {
     public Optional<Member> findOne(Long memeberId){
         return memberRepository.findById(memeberId);
     }
-
 
 }
